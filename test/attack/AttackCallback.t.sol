cc5f466789585fb76d017289886a2de1d9a5cf08
SunnyBoyy95 <popovivan9503@gmail.com>
2024-05-25 10:02:42 +0300

    Update AttackCallback.t.sol

--- test/attack/AttackCallback.t.sol
+++ test/attack/AttackCallback.t.sol
@@ -1,32 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "../pool/Setup.t.sol";
 import "../mocks/AttackCallbackContract.sol";
 
 contract TestAttackCallback is TestPool {
-    AttackCallbackContract _attackCountract;
-    uint256 pairId;
+    AttackCallbackContract private _attackContract; // Corrected typo
+    uint256 private pairId; // Marked as private
 
     function setUp() public override {
-        TestPool.setUp();
+        super.setUp(); // Corrected method call
 
         pairId = registerPair(address(currency1), address(0), false);
 
         predyPool.supply(pairId, true, 1e8);
         predyPool.supply(pairId, false, 1e8);
 
-        _attackCountract = new AttackCallbackContract(predyPool, address(currency1));
+        _attackContract = new AttackCallbackContract(predyPool, address(currency1)); // Corrected typo
     }
 
     function testAttackCallback() public {
-        assertEq(currency1.balanceOf(address(_attackCountract)), 0);
+        assertEq(currency1.balanceOf(address(_attackContract)), 0);
 
-        IPredyPool.TradeParams memory tradeParams = IPredyPool.TradeParams(pairId, 0, 0, 0, bytes("0x"));
+        IPredyPool.TradeParams memory tradeParams = IPredyPool.TradeParams({
+            pairId: pairId,
+            tradeType: 0,
+            quantity: 0,
+            price: 0,
+            data: bytes("0x")
+        });
 
         vm.expectRevert(bytes("ReentrancyGuard: reentrant call"));
-        _attackCountract.trade(tradeParams);
+        _attackContract.trade(tradeParams);
 
-        assertEq(currency1.balanceOf(address(_attackCountract)), 0);
+        assertEq(currency1.balanceOf(address(_attackContract)), 0);
     }
 }
